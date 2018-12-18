2a97475c973996c034cbf67795f9c7049ef3e33e
Alexis Paques <alexis.paques@gmail.com>
2018-12-18 09:30:23 +0100

    Change teraranger bus

--- src/drivers/distance_sensor/teraranger/teraranger.cpp
+++ src/drivers/distance_sensor/teraranger/teraranger.cpp
@@ -71,7 +71,7 @@
 #include <board_config.h>
 
 /* Configuration Constants */
-#define TERARANGER_BUS           PX4_I2C_BUS_EXPANSION
+#define TERARANGER_BUS           PX4_I2C_BUS_ONBOARD
 #define TRONE_BASEADDR      0x30 /* 7-bit address */
 #define TREVO_BASEADDR      0x31 /* 7-bit address */
 #define TERARANGER_DEVICE_PATH   	"/dev/teraranger"
